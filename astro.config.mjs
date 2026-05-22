2b0d8398c4a8a777ad4cc82d93240c5315dc4dac
fakefa <farreraspol@hotmail.com>
2026-05-22 22:39:30 +0200

    added files to deploy web on github

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: "https://fakefarreraspol.github.io/",
+  base: '/fakefarreraspol.github.io',
 });
